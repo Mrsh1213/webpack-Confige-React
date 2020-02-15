ea048ead79288efbae9ae522d7e58dea3311c4d4
mrsh1213 <mohammad.shobiri@gmail.com>
2020-02-15 17:40:47 +0330

    Initial commit

---
+++ command.sh
@@ -0,0 +1,6 @@
+touch .babelrc
+sudo npm i react react-dom --save^C
+sudo npm install --save-dev webpack webpack-cli webpack-dev-server
+sudo npm install --save-dev @babel/core babel-loader @babel/preset-env @babel/preset-react
+sudo npm install --save-dev file-loader url-loader
+
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+
+function App() {
+    return (
+            <div>Welcome to my-webpack-react-starter</div>
+    );
+}
+ReactDOM.render(<App/>, document.querySelector("#root"));
